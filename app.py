3061f58e24b70349ec66152a3477674c66f6aff4
shenamariee <23-06459@g.batstate-u.edu.ph>
2025-12-08 01:53:07 +0800

    Fix indentation for last_analysis check

--- app.py
+++ app.py
@@ -577,7 +577,7 @@ st.header("Result Summary")
 st.write("Your Result Are Here:")
 
 # Show the overall summary generated in your analysis section
-    if not st.session_state['last_analysis']: 
+if not st.session_state['last_analysis']: 
     st.info("No analysis run yet — go to ECG Upload & Analysis and run the analysis.") 
     return res = st.session_state['last_analysis'] 
     overall_summary = res['overall_summary']
