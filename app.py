766be5021b74cf3fa197434181644ad931f54dc7
shenamariee <23-06459@g.batstate-u.edu.ph>
2025-12-08 01:33:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -575,7 +575,7 @@ def show_ecg_analysis():
 st.header("Your Results Are Here:")
 
 # Show the overall summary generated in your analysis section
-st.write(f"**ECG Summary:** {last_analysis}")
+st.write(f"**ECG Summary:** {overall_summary}")
 
 # Conditional explanation messages
 if overall_summary.lower() == "normal":
